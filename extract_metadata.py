1ec202d317f3dd1296f68dd351488d10ffa62f01
MartinPC-uls <martin.pizarro@userena.cl>
2024-01-09 15:40:12 -0300

    now HTML 'dir' tag is being extracted as well

--- extract_metadata.py
+++ extract_metadata.py
@@ -41,6 +41,7 @@ def get_metadata(warc_path, max_count=0, remove=False):
                     'Domain': get_domain_from_url(warc_target_uri).split('.')[-1],
                     'Content-Language': content_language,
                     'HTML-Language': extract_lang(content),
+                    'HTML-Dir': extract_dir(content)
                 })
 
                 i += 1
--- utils.py
+++ utils.py
@@ -29,6 +29,22 @@ def extract_lang(html: str):
     lang_tag = soup.find('html', {'lang': True}) or soup.find('html', {'xml:lang': True})
     return lang_tag.get('lang') or lang_tag.get('xml:lang') if lang_tag else None
 
+def extract_dir(html: str):
+    """
+    Extracts the HTML Dir Tag if found. Otherwise it returns None.
+
+    Parameters:
+    - html (str): HTML code as string.
+
+    Returns:
+    str: HTML Dir.
+    """
+    if html == '': return None
+
+    soup = BeautifulSoup(html, "html.parser", from_encoding="iso-8859-1")
+
+    dir_tag = soup.find('html', {'dir': True})
+    return dir_tag.get('dir') if dir_tag else None
 
 def get_header(name: str, http_headers, default_value=None, exact_match=True):
         """
